f225769a0eadb059586a1c44770cc1453cbf9f9d
mozesi <msukwamose1@gmail.com>
2023-10-26 17:59:59 +0200

    worked on the landing page

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,8 +8,27 @@
         @vite('resources/css/app.css')
     </head>
     <body class="antialiased">
-        <div>
+        <nav class="relative container mx-auto p-6">
+            <div class="flex items-center justify-between">
+                <div class="pt-2">
+                    <img src="storage/logo-black.png" alt="moses msukwa">
+                </div>
+                <div class="hidden md:flex space-x-6">
+                    <a href="#">Education</a>
+                    <a href="#">Experience</a>
+                    <a href="#">Certifications</a>
+                    <a href="#">Projects</a>
+                    <a href="#">Articles</a>
+                </div>
+                <a href="#" class=" hidden md:block p-3 px-6 pt-2 text-white bg-red-500 rounded-full baseline hover:bg-red-300">
+                    Contact me
+                </a>
+            </div>
+        </nav>
+
+        <div >
             {{$slot}}
         </div>
+
     </body>
 </html>
--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -1,18 +1,32 @@
-<div class="container mx-auto">
-    <div class="columns-3">
-        <p>
-            Hello my Name is {{$myProfile->first_name}} {{$myProfile->middle_name}} {{$myProfile->last_name}}
-        </p>
-        <p>
-            {{$myProfile->bio}}
-        </p>
-        <p>
-            <button class="btn">Resume</button>
-        </p>
+<div>
+    <div class="container flex flex-col-reverse md:flex-row items-center px-6 mx:auto mt-10 space-y-0 md:space-y-0">
+        <div class="flex flex-col mb-32 space-y-12 md:w-1/2">
+            <h1 class="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
+                Hi! my name is {{$myProfile->first_name}} {{$myProfile->last_name}}
+             </h1>
+             <h1 class="max-w-sm text-center text md:text-left">
+                 {{$myProfile->bio}}
+             </h1>
+             <div class="flex justify-center md:justify-start">
+                 <a href="#" class=" hidden md:block p-3 px-6 pt-2 text-white bg-red-500 rounded-full baseline hover:bg-red-300">
+                     Download resume
+                 </a>
+             </div>
+        </div>
+        <div class="md:w-1/2">
+            <img src="storage/{{$myProfile->profile_picture}}" alt="{{$myProfile->first_name}}{{$myProfile->last_name}}">
+        </div>
     </div>
-    <div>
-        <p><img src="#" alt="Moses Msukwa"></p>
+    <div class="container flex flex-col-reverse md:flex-row items-center px-6 mx:auto mt-10 space-y-0 md:space-y-0">
+        <div class="flex flex-col mb-32 space-y-12 md:w-1/2">
+            <h1 class="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
+                My recent Projects
+            </h1>
+            <p class="max-w-sm text-center text-black md:text-left">
+                Below is the list of my projects.
+            </p>
+        </div>
     </div>
-
 </div>
+
 
