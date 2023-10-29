ee523663c0f86a004c7357fb8b902168c967742d
mozesi <msukwamose1@gmail.com>
2023-10-29 08:31:25 +0200

    created more site livewire componets

---
+++ app/Livewire/Certification.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Certification extends Component
+{
+    public function render()
+    {
+        return view('livewire.certification');
+    }
+}
---
+++ app/Livewire/Experience.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Experience extends Component
+{
+    public function render()
+    {
+        return view('livewire.experience');
+    }
+}
