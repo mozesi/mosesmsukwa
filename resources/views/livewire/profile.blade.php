cea5e0504d4bbb5c57d9b75df3dc037089b8a350
mozesi <msukwamose1@gmail.com>
2025-03-02 08:47:11 +0200

    Using herd

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -1,6 +1,7 @@
 <div>
-    <div class="container flex flex-col-reverse md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0">
-        <div class="flex flex-col mb-32 space-y-12 md:w-1/2">
+
+    <div class="  bg-blue-700 flex flex-col-reverse md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0">
+        <div class=" flex flex-col mb-32 space-y-12 md:w-1/2">
             <h1 class="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
                 Hi! my name is {{$myProfile->first_name}} {{$myProfile->last_name}}
              </h1>
@@ -10,12 +11,12 @@
              <div class="flex justify-center md:justify-start">
                 <a href="/contact" wire:navigate>
                     <button class="p-3 px-6 pt-2 text-white bg-black rounded-full baseline ">
-                        Contact me
+                        Get in touch
                     </button>
                 </a>
              </div>
         </div>
-        
+
 
         <div class="md:w-1/2">
             <img src="{{ asset('images/background.jpg') }}" alt="{{$myProfile->first_name}}{{$myProfile->last_name}}">
